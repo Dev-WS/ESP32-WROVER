2ba3e45f2168d02c595f1a4c81f6eab536ecfd3e
Dev-WS <wojstr@gmail.com>
2020-11-29 14:01:32 +0100

    First Commit

--- src/main.c
+++ src/main.c
@@ -215,7 +215,7 @@ void app_main(void)
 {
 	i2c_master_init();
 	ssd1306_init();
-    
+
     
 
 	//xTaskCreate(&task_ssd1306_display_pattern, "ssd1306_display_pattern",  2048, NULL, 6, NULL);
@@ -223,8 +223,6 @@ void app_main(void)
 	vTaskDelay(100/portTICK_PERIOD_MS);
 	 xTaskCreate(&task_ssd1306_display_text, "ssd1306_display_text",  2048,
 	 	(void *)"R", 6, NULL);
-
-
     //  xTaskCreate(&task_ssd1306_display_text, "ssd1306_display_text",  2048,
 	//  	(void *)"Test", 6, NULL);
 	//xTaskCreate(&task_ssd1306_contrast, "ssid1306_contrast", 2048, NULL, 6, NULL);
