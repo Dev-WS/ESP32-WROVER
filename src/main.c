46545b7a2f79d978c67a7b57addaa43dc08ccb8e
Dev-WS <wojstr@gmail.com>
2020-11-29 13:45:33 +0100

    First Commit

--- src/main.c
+++ src/main.c
@@ -216,6 +216,7 @@ void app_main(void)
 	i2c_master_init();
 	ssd1306_init();
     
+    
 
 	//xTaskCreate(&task_ssd1306_display_pattern, "ssd1306_display_pattern",  2048, NULL, 6, NULL);
 	xTaskCreate(&task_ssd1306_display_clear, "ssd1306_display_clear",  2048, NULL, 6, NULL);
