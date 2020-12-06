391f6715a9cdc01294cd0e1aeed7240d347b5c63
Dev-WS <wojstr@gmail.com>
2020-12-06 22:04:28 +0100

    added StartScheduler

--- src/main.c
+++ src/main.c
@@ -10,14 +10,22 @@ void app_main(void)
 	ssd1306_init();
 	bme280();
  
-	xTaskCreate(&task_bme280, "bme280_normal_mode",  2048, NULL, 5, NULL);
+	//xTaskCreate(&task_ssd1306_display_pattern, "ssd1306_display_pattern",  2048, NULL, 6, NULL);
+
+	xTaskCreate(&task_bme280, "bme280_mode",  2048, NULL, 6, NULL);
 	
 	xTaskCreate(&task_ssd1306_display_clear, "ssd1306_display_clear",  2048, NULL, 6, NULL);
 	vTaskDelay(100/portTICK_PERIOD_MS);
-
+	// xTaskCreate(&task_ssd1306_display_text, "ssd1306_display_text",  2048,
+	// 	(void *)"R", 6, NULL);
     xTaskCreate(&task_ssd1306_display_text, "ssd1306_display_text",  2048,
-	  	(void *)strTemperature, 6, NULL);
+	  	(void *)strTemperature, 5, NULL);
+	vTaskDelay(10/portTICK_PERIOD_MS);
+	
 	//xTaskCreate(&task_ssd1306_contrast, "ssid1306_contrast", 2048, NULL, 6, NULL);
 	//xTaskCreate(&task_ssd1306_scroll, "ssid1306_scroll", 2048, NULL, 6, NULL);
 	
+	vTaskStartScheduler();
+
+	//for(;;);
 }
